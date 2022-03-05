818c5d1f88fb355a328131fce66244b4b48cb920
Cesar Acual <augustoacual@gmail.com>
2022-03-04 19:21:18 -0600

    Cargar tareas

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ const main = async () => {
 
   if (tareasDB) {
     // Establecer tareas
+    tareas.cargarTareasFromArray(tareasDB);
   }
-  await pausa();
+
   do {
     opt = await inquirerMenu();
 
@@ -31,7 +32,7 @@ const main = async () => {
         break;
     }
 
-    // guardarDB(tareas.listadoArr);
+    guardarDB(tareas.listadoArr);
 
     await pausa();
   } while (opt !== "0");
